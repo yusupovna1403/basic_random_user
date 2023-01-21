3fd42a59bde160a3d7df06b1cb0572b1fa1fae0e
yusupovna1403 <yusupovna2003girl@gmail.com>
2023-01-21 18:45:05 +0500

    json_

--- main.py
+++ main.py
@@ -2,4 +2,7 @@ import json
 
 f= open('data.json').read()
 data = json.loads(f)
-print(data)
+lst = data['results']
+first_name = lst[0]['name']['first']
+last_name = lst[0]['name']['last'] 
+print(first_name,last_name)
